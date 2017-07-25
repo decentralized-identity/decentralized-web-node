c70505ee1b2a819d891c2abf7a7e07471eb0ee00
Matt Smith <matt.the.smith@gmail.com>
2017-07-25 10:59:20 -0700

    removing compiled js files

--- implementations/node/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var api_1 = require("./api");
-var app_1 = require("./config/app");
-api_1["default"].listen(app_1["default"].port, function () {
-    console.log("Your app is running at " + app_1["default"].baseURL);
-});
