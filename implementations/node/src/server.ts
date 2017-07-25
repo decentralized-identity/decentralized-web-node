002c725b33def5aadf9cd567d833722c35f1f2b9
Daniel Buchner <danieljb2@gmail.com>
2017-07-25 13:09:43 -0700

    WIP

--- implementations/node/src/server.ts
+++ implementations/node/src/server.ts
@@ -1,8 +1,6 @@
 import app from './api';
 import appConfig from './config/app';
 
-const nano = require('nano')(appConfig.dbURL);
-
 app.listen(appConfig.port, () => {
   console.log(`Your app is running at ${appConfig.baseURL}`);
 });
