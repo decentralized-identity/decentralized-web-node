5ffd3b80b404a1de2093192a6aeca13d73b7c731
Daniel Buchner <danieljb2@gmail.com>
2017-08-08 07:51:12 -0700

    working header and body requests, with syntax highlighting

--- implementations/node/src/api/controllers/_dev/index.ts
+++ implementations/node/src/api/controllers/_dev/index.ts
@@ -15,16 +15,32 @@ devRouter.get('/playground', async ctx => {
   });
 });
 
-devRouter.get('/playground/playground.css', async ctx => {
-  await new Promise((resolve, reject) => {
-    fs.readFile('./test/playground/playground.css', function(error, content) {
-      console.log(error);
-      if (error) reject();
-      ctx.set('Content-Type', 'text/css');
-      ctx.body = content;
-      resolve();
+const files = {
+  'text/javascript': [
+    '/playground/playground.js',
+    '/playground/editor/prism.js',
+    '/playground/editor/codeflask.js'
+  ],
+  'text/css': [
+    '/playground/playground.css',
+    '/playground/editor/prism.css',
+    '/playground/editor/codeflask.css'
+  ]
+};
+
+for (let type in files) {
+  files[type].forEach(file => {
+    devRouter.get(file, async ctx => {
+      await new Promise((resolve, reject) => {
+        fs.readFile('./test' + file, function(error, content) {
+          if (error) reject();
+          ctx.set('Content-Type', type);
+          ctx.body = content;
+          resolve();
+        });
+      });
     });
   });
-});
+}
 
 export default devRouter;
