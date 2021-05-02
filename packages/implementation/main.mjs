5d8c323bcf1aa817c3433dc6fce3875358dbffd9
Daniel Buchner <danieljb2@gmail.com>
2021-05-02 10:58:11 -0700

    First swing at spec skeleton

---
+++ packages/implementation/main.mjs
@@ -0,0 +1,11 @@
+
+// import * as fs from 'fs';
+// import { Context } from '@textile/context';
+// import { Client } from '@textile/hub';
+
+// console.log(Context);
+
+// const config = JSON.parse(fs.readFileSync('config.json'));
+// const client = new Client(new Context('localhost:4006'))
+
+// console.log(client);
